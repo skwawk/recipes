9a04e544cba5ea7fa39a4162fee482396a2d0e79
skwawk <brainybrack@gmail.com>
2016-11-03 18:50:25 -0400

    Update recipeBook.js

--- recipeBook.js
+++ recipeBook.js
@@ -16,26 +16,26 @@ var bookLength = recipes.length;
 
 function printRecipe(recipe) {
     console.log(recipe.name);
-    console.log()
+    console.log();
     console.log(recipe.ingredients);
-    console.log()
+    console.log();
     console.log(recipe.steps);
-}
+};
 
 function list() {
 	for (var i = 0; i < bookLength; i++) {
 		printRecipe(recipes[i]);
-	}
-}
+	};
+};
 
 function search (name) {
 	name = name.toLowerCase();
     for (var i = 0; i < bookLength; i++) {
         if (recipes[i].name === name) {
             printRecipe(recipes[i]);
-        }
-    }
-}
+        };
+    };
+};
 
 function add (name, ingredients, steps) {
     recipes[recipes.length] = {
@@ -43,4 +43,5 @@ function add (name, ingredients, steps) {
         ingredients: ingredients,
         steps: steps
     };
-}
+};
+list();
