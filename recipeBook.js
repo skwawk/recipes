1f7936119e29b7158b58cc2dd1299f02791531b3
skwawk <brainybrack@gmail.com>
2016-11-03 19:43:19 -0400

    Update recipeBook.js

--- recipeBook.js
+++ recipeBook.js
@@ -1,20 +1,20 @@
 chocCake = {
 	name: "chocolate cake",
-	ingredients: "2 cups white sugar\n1 3/4 cups all-purpose flour\n3/4 cup unsweetened cocoa powder\n" +
-	"1 1/2 teaspoons baking powder\n1 1/2 teaspoons baking soda\n1 teaspoon salt\n2 eggs\n1 cup milk\n" +
-	"1/2 cup vegetable oil\n2 teaspoons vanilla extract\n1 cup boiling water",
-	steps: "1: Preheat oven to 350 degrees F (175 degrees C). Grease and flour two nine inch round pans.\n" + "\n" +
+	ingredients: "2 cups white sugar<br />1 3/4 cups all-purpose flour<br />3/4 cup unsweetened cocoa powder<br />" +
+	"1 1/2 teaspoons baking powder<br />1 1/2 teaspoons baking soda<br />1 teaspoon salt<br />2 eggs<br />1 cup milk" +
+	"<br />1/2 cup vegetable oil<br />2 teaspoons vanilla extract<br />1 cup boiling water",
+	steps: "1: Preheat oven to 350 degrees F (175 degrees C). Grease and flour two nine inch round pans.<br /><br />" +
 	"2: In a large bowl, stir together the sugar, flour, cocoa, baking powder, baking soda and salt. Add the eggs, " +
 	"milk, oil and vanilla, mix for 2 minutes on medium speed of mixer. Stir in the boiling water last. Batter will " +
-	"be thin. Pour evenly into the prepared pans.\n" + "\n" +
+	"be thin. Pour evenly into the prepared pans.<br /><br />" +
 	"3: Bake 30 to 35 minutes in the preheated oven, until the cake tests done with a toothpick. Cool in the pans " + 
 	"for 10 minutes, then remove to a wire rack to cool completely.",
 };
 chocFrost = {
 	name: "chocolate frosting",
-	ingredients: "2 3/4 cups confectioners' sugar\n6 tablespoons unsweetened cocoa powder\n6 tablespoons butter\n" +
- 	"5 tablespoons evaporated milk\n1 teaspoon vanilla extract",
-	steps: "1: In a medium bowl, sift together the confectioners' sugar and cocoa, and set aside.\n" + "\n" +
+	ingredients: "2 3/4 cups confectioners' sugar<br />6 tablespoons unsweetened cocoa powder<br />" +
+	"6 tablespoons butter<br />5 tablespoons evaporated milk<br />1 teaspoon vanilla extract",
+	steps: "1: In a medium bowl, sift together the confectioners' sugar and cocoa, and set aside.<br /><br />" +
 	"2: In a large bowl, cream butter until smooth, then gradually beat in sugar mixture alternately with evaporated " +
 	"milk. Blend in vanilla. Beat until light and fluffy. If necessary, adjust consistency with more milk or sugar.",
 	full: this.ingredients + this.steps
