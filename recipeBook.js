abe3434e1020d9a2107c929bde5437b94b38608e
skwawk <brainybrack@gmail.com>
2016-11-05 08:20:03 -0400

    Update recipeBook.js

--- recipeBook.js
+++ recipeBook.js
@@ -12,36 +12,23 @@ function Recipe(id, name, ingredients, steps,) {
     		console.log(this.steps);
 	};
 	this.update = function () {
-		recipeDiv = function.getElementById(name);
+		recipeDiv = document.getElementById(name);
 		recipeDiv.innerHTML = this.title + this.ingredients + "<br /><br />" + this.steps;
 	};
 	this.hide = function() {
-		recipeDiv = function.getElementById(name);
+		recipeDiv = document.getElementById(name);
 		recipeDiv.innerHTML = "";
 	};
 	var btn = document.createElement("input");
-	document.body.appendChild(btn);	
 	btn.type = "button";
-	btn.name = id;
-	btn.id = id;
+	btn.name = this.id;
 	btn.value = this.name;
-	btn.onclick = this.update();
-	
+	btn.onclick = this.update;
+  	document.body.appendChild(btn);
 	var div = document.createElement("div");
+  	div.id = this.id;
+  	document.body.appendChild(btn);
 	
-};
-
-chocCake = {
-	name: "Chocolate cake",
-	ingredients: "2 cups white sugar<br />1 3/4 cups all-purpose flour<br />3/4 cup unsweetened cocoa powder<br />" +
-	"1 1/2 teaspoons baking powder<br />1 1/2 teaspoons baking soda<br />1 teaspoon salt<br />2 eggs<br />1 cup milk" +
-	"<br />1/2 cup vegetable oil<br />2 teaspoons vanilla extract<br />1 cup boiling water",
-	steps: "1: Preheat oven to 350 degrees F (175 degrees C). Grease and flour two nine inch round pans.<br /><br />" +
-	"2: In a large bowl, stir together the sugar, flour, cocoa, baking powder, baking soda and salt. Add the eggs, " +
-	"milk, oil and vanilla, mix for 2 minutes on medium speed of mixer. Stir in the boiling water last. Batter will " +
-	"be thin. Pour evenly into the prepared pans.<br /><br />" +
-	"3: Bake 30 to 35 minutes in the preheated oven, until the cake tests done with a toothpick. Cool in the pans " + 
-	"for 10 minutes, then remove to a wire rack to cool completely.",
 };
 
 var recipes = [ ];
